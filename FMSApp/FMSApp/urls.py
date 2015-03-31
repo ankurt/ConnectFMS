e55d1cda01798935f82131d9cd46bdd0357b3814
Ankur Toshniwal <ankur@getfbpay.in>
2015-03-31 15:39:34 -0400

    urls.py

--- FMSApp/FMSApp/urls.py
+++ FMSApp/FMSApp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-     url(r'^Connect_FMS/', include('Connect_FMS.urls')),
+    url(r'^Connect_FMS/', include('Connect_FMS.urls')),
 
 )
