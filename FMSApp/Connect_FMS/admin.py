514463d672f147f7b89b454671730a5997eb8bbb
Ankur Toshniwal <ankur@getfbpay.in>
2015-04-01 17:14:48 -0400

    Working on feed and comments

--- FMSApp/Connect_FMS/admin.py
+++ FMSApp/Connect_FMS/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Building, Location, Utility, User
+from .models import Building, Location, Utility, User, Post
 
 
 admin.site.register(Building)
 admin.site.register(Location)
 admin.site.register(Utility)
 admin.site.register(User)
+admin.site.register(Post)
--- FMSApp/Connect_FMS/models.py
+++ FMSApp/Connect_FMS/models.py
@@ -21,6 +21,7 @@ STATUS_CHOICES = (
 STATES_CHOICES = (('AL', 'Alabama'), ('AK', 'Alaska'), ('AZ', 'Arizona'), ('AR', 'Arkansas'), ('CA', 'California'), ('CO', 'Colorado'), ('CT', 'Connectict'), ('DE', 'Delaware'), ('DC', 'District of Columbia '), ('FL', 'Florida'), ('GA', 'Georgia'), ('HI', 'Hawaii'), ('ID', 'Idaho'), ('IL', 'Illinois'), ('IN', 'Indiana'), ('IA', 'Iowa'), ('KS', 'Kansas'), ('KY', 'Kentucky'), ('LA', 'Louisiana'), ('ME', 'Maine'), ('MD', 'Maryland'), ('MA', 'Massachusetts'), ('MI', 'Michigan'), ('MN', 'Minnesota'), ('MS', 'Mississippi'), ('MO', 'Missouri'), ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'), ('NH', 'New Hampshire'), ('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NY', 'New York'), ('NC', 'North Carolina'), ('ND', 'North Dakota'), ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'), ('PA', 'Pennsylvania'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'), ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'), ('VT', 'Vermont'), ('VA', 'Virginia'), ('WA', 'Washington'), ('WV', 'West Virginia'), ('WI', 'Wisconsin '), ('WY', 'Wyoming'))
 
 
+
 class FMSUserManager(models.Manager):
     def get_queryset(self):
         return super(FMSUserManager, self).get_queryset().filter(role='fms').order_by('last_name', 'first_name')
